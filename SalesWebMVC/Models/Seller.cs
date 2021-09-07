145af7e4963d70675094cd54c21c20cce991abad
ArethaMartins <aretha.martins@gestaosat.com>
2021-09-06 21:13:24 -0300

    App locale, number and date formatting

--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 
 namespace SalesWebMVC.Models
@@ -8,16 +9,24 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
 
+        [Display(Name = "Nome")]
         public string Name { get; set; }
 
+        [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
 
+        [Display(Name = "Data de nascimento")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
 
+        [Display(Name = "Salário base")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
-
+        
         public Department Department { get; set; }
 
+        [Display(Name = "Departamento")]
         public int DepartmentId { get; set;}
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
